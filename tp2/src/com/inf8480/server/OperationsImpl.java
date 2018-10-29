909ec24b9f43e14f2bb09b370adc8523d1058ffd
FX Dueymes <francois-xavier.dueymes@polymtl.ca>
2018-10-29 14:55:59 -0400

    added server malicious behavior. Maliciousness is an optional parameter set to 0 by default

--- tp2/src/com/inf8480/server/OperationsImpl.java
+++ tp2/src/com/inf8480/server/OperationsImpl.java
@@ -2,16 +2,42 @@ package com.inf8480.server;
 
 import com.inf8480.common.Operations;
 
+import java.util.Random;
+
 
 public class OperationsImpl implements Operations {
 
+
+    private int _maliciousness;
+    private boolean _maliciousMode = false;
+    private Random rand;
+
+    private int temperResult(int result) {
+        int randomNum = rand.nextInt(100 + 1);
+        boolean decision = randomNum < _maliciousness;
+        System.out.println("randomNum " + randomNum);
+        System.out.println("temperResult ? " + decision);
+        if(decision) {
+            result += randomNum;
+        }
+        return result;
+    }
+
+     OperationsImpl(int maliciousness) {
+        super();
+        rand = new Random();
+        _maliciousness = maliciousness;
+        if (_maliciousness > 0) {
+            _maliciousMode = true;
+        }
+    }
     @Override
     public int sum(int[] results) {
         int sum = 0;
         for (int result: results) {
             sum = (sum + result) % 4000;
         }
-        return sum;
+        return (_maliciousMode) ? temperResult(sum) : sum;
     }
     /**
      * Methodes utilitaires pour effectuer les operations du TP2.
@@ -23,12 +49,19 @@ public class OperationsImpl implements Operations {
      */
     @Override
     public int pell(int x) {
+        int result = computePell(x);
+        return (_maliciousMode) ? temperResult(result) : result;
+    }
+
+    private int computePell(int x) {
         if (x == 0)
             return 0;
         if (x == 1)
             return 1;
-        return 2 * pell(x - 1) + pell(x - 2);
+        return 2 * computePell(x - 1) + computePell(x - 2);
     }
+
+
 
     @Override
     public int prime(int x) {
@@ -40,7 +73,7 @@ public class OperationsImpl implements Operations {
                 highestPrime = i;
         }
 
-        return highestPrime;
+        return (_maliciousMode) ? temperResult(highestPrime) : highestPrime;
     }
 
     private static boolean isPrime(int x) {
--- tp2/src/com/inf8480/server/Server.java
+++ tp2/src/com/inf8480/server/Server.java
@@ -9,12 +9,19 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class Server {
 
+
     public Server() {}
 
     public static void main(String[] args) {
         try {
+            int maliciousness;
+            if (args.length > 0) {
+                maliciousness = Integer.parseInt(args[0]);
+            } else {
+                maliciousness = 0;
+            }
             // Instantiating the implementation class
-            OperationsImpl obj = new OperationsImpl();
+            OperationsImpl obj = new OperationsImpl(maliciousness);
 
             // Exporting the object of implementation class
             // (here we are exporting the remote object to the stub)
@@ -27,7 +34,9 @@ public class Server {
             Registry registry = LocateRegistry.createRegistry(5001);
 
             registry.rebind("Operations", skeleton);
-            System.err.println("Server ready");
+            System.out.println("Server ready, maliciousness is set to " + maliciousness);
+
+
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();

13cad7fff4ee13b6d9157c1e3f04b3dae250211f
François-Xavier Dueymes <frdue@l4712-15.info.polymtl.ca>
2018-12-04 14:00:23 -0500

    part 2 completed

--- tp3/request_loop.sh
+++ tp3/request_loop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for i in {1..30}
 do
-   curl -s http://132.207.12.225:8080/?nom=Yvon && echo "request $i done" &
+# POUR TESTER: Adresse IP flottante a recuperer dans outputs de la vue densemble du stack 
+   curl -s http://132.207.12.106:8080/?nom=Yvonne && echo "request $i done" &
 done
 
 wait
